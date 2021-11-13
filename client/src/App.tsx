90b89f62ae36f9d6ecaa59a48ef70f281930cd00
JacksonTrudel <jtrudel97@gmail.com>
2021-11-13 05:55:46 +0000

    Added react-router components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,15 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Homepage from './containers/Homepage';
 
 function App() {
     return (
         <div className="App">
-            <Homepage />
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Homepage />} />
+                </Routes>
+            </Router>
         </div>
     );
 }
